980250dd919cca7a2d1b7619050c1e541ed6843e
raoawais888 <raoawais888@gmail.com>
2023-02-16 22:12:01 +0500

    frontend

--- routes/web.js
+++ routes/web.js
@@ -4,6 +4,10 @@ import HomeController from "../controllers/HomeController.js";
 const router = express.Router();
 
     router.get("/",HomeController.index);
+    router.get("/about",HomeController.about);
+    router.get("/product",HomeController.product);
+    router.get("/why",HomeController.why);
+    router.get("/testimonial",HomeController.testimonial);
 
 
 export default router;
