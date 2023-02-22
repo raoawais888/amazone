a2a921befbb2d25fa714749a8c92005d785efddc
unknown <waqasarif588@gmail.com>
2023-02-22 22:01:07 +0500

    waqas

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,6 +1,11 @@
 import product from "../models/productModel.js"
 class cartController {
     static cart = async (req,res)=> {
-            
+            const cart_id  = req.params.id;
+            // const store = localStorage.setItem('cart',cart_id)
+            const store = req.cookie
+            console.log(store);
     }
 }
+
+export default cartController
